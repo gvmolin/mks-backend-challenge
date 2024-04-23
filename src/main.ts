7d1b6e0781512d2b3ca182b2b7d37fce56812ec8
Gabriel Molin <molinguin@gmail.com>
2024-04-23 10:43:12 -0300

    feat: main changes, swagger, database, etc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import * as dotenv from "dotenv";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import helmet from 'helmet';
+import { DatabaseUtils } from './infrastructure/utils/database/database.utils';
 
+dotenv.config();
 async function bootstrap() {
+
+  const dbUtils = new DatabaseUtils();
+  await dbUtils.initializeDB();
+  
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+  .setTitle("MKS CHALLENGE - Backend")
+  .setDescription("API do desafio mks")
+  .addBearerAuth({ type: "http", scheme: "bearer", bearerFormat: "JWT" })
+  .setVersion("v1")
+  .setExternalDoc('Arquivo de Configuração', '/api/docs-json')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  const enviroment = process.env.NODE_ENV.toLowerCase();
+  const helmetEnabledCondition = enviroment !== "dev";
+  console.log(helmetEnabledCondition);
+
+  if (helmetEnabledCondition) app.use(helmet());
+  else SwaggerModule.setup("/api/docs", app, document);
+
+  await app.listen(process.env.APP_PORT, () =>
+    console.log(`Server started on port: ${process.env.APP_PORT}`)
+  );
 }
 bootstrap();
